b3968454ae8e06196d54486f7e0f958262898b8f
iratxe <iiratxe.ggonzalez@gmail.com>
2021-04-23 18:18:39 +0200

    Archivos restantes.

---
+++ app/src/main/java/com/example/dontstopthemusic/Main/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.dontstopthemusic.Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.dontstopthemusic.Login_Registro.LoginActivity;
+import com.example.dontstopthemusic.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setSupportActionBar(findViewById(R.id.labarraMain));
+
+        // Botón para ir a LOGIN
+        Button btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iLogin = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(iLogin);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menumain,menu);
+        return true;
+    }
+}
